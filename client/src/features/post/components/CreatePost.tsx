3d96d40fada655c57afacf63f8979869cae1b5e7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-02-25 23:07:47 +0200

    Configure image upload functionality

--- client/src/features/post/components/CreatePost.tsx
+++ client/src/features/post/components/CreatePost.tsx
@@ -5,13 +5,39 @@ import { uploadImage } from "../services/uploadImage";
 import "./CreatePost.scss";
 
 const CreatePost = () => {
+  const [imageSrc, setImageSrc] = useState<String | ArrayBuffer | null>("");
+  const [uploadData, setUploadData] = useState<Object>();
+
+  const handleOnChange = (changeEvent: React.ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader();
+
+    reader.onload = function (onLoadEvent) {
+      if (onLoadEvent.target) {
+        setImageSrc(onLoadEvent.target.result);
+      }
+      setUploadData(undefined);
+    };
+
+    if (changeEvent.target.files) {
+      reader.readAsDataURL(changeEvent.target.files[0]);
+    }
+  };
+
+  const handleOnSubmit = async (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+
+    const { imageUrl, data } = await uploadImage(ev);
+
+    setImageSrc(imageUrl);
+    setUploadData(data);
+  };
 
   return (
     <main className="main__create">
       <form
         action="POST"
         className="main__create--form create__form"
-       
+        onSubmit={handleOnSubmit}
       >
         <legend className="create__form--legend legend">Create Post</legend>
         <textarea
@@ -20,7 +46,7 @@ const CreatePost = () => {
           className="create__form--textarea create__content content textarea"
         />
         <p>
-          <input type="file" name="file"/>
+          <input type="file" name="file" onChange={handleOnChange} />
         </p>
         <button
           type="submit"
