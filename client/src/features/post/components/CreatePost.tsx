cdf7f8b8b82679a4246b2e83fa8803d4686234e9
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-02-26 00:01:52 +0200

    Implement create post functionality

--- client/src/features/post/components/CreatePost.tsx
+++ client/src/features/post/components/CreatePost.tsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { createPost } from "../api/post-api";
 
 import { uploadImage } from "../services/uploadImage";
 
 import "./CreatePost.scss";
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [postContent, setPostContent] = useState("");
   const [imageSrc, setImageSrc] = useState<String | ArrayBuffer | null>("");
-  const [uploadData, setUploadData] = useState<Object>();
+
+  const handleContentChange = (ev: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setPostContent(ev.target.value.split("\n").join("<br/>"));
+  };
 
   const handleOnChange = (changeEvent: React.ChangeEvent<HTMLInputElement>) => {
     const reader = new FileReader();
@@ -15,7 +22,6 @@ const CreatePost = () => {
       if (onLoadEvent.target) {
         setImageSrc(onLoadEvent.target.result);
       }
-      setUploadData(undefined);
     };
 
     if (changeEvent.target.files) {
@@ -26,10 +32,19 @@ const CreatePost = () => {
   const handleOnSubmit = async (ev: React.FormEvent<HTMLFormElement>) => {
     ev.preventDefault();
 
-    const { imageUrl, data } = await uploadImage(ev);
+    let imageUrl = "";
+    if (imageSrc) {
+      imageUrl = await uploadImage(ev);
+    }
 
-    setImageSrc(imageUrl);
-    setUploadData(data);
+    const post = await createPost({
+      postContent,
+      imageSrc: imageUrl,
+    });
+
+    if (post) {
+      navigate("/");
+    }
   };
 
   return (
@@ -44,10 +59,14 @@ const CreatePost = () => {
           name="postContent"
           placeholder="What do you think?"
           className="create__form--textarea create__content content textarea"
+          onChange={handleContentChange}
         />
-        <p>
-          <input type="file" name="file" onChange={handleOnChange} />
-        </p>
+        <input
+          type="file"
+          name="file"
+          onChange={handleOnChange}
+          className="create__form--file create__file file"
+        />
         <button
           type="submit"
           className="create__form--button create__button button__element button"
