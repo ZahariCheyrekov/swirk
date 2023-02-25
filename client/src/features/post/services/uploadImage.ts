9f2ad864723deb6cb704151145b650eedd0ecea0
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-02-25 23:06:23 +0200

    Implement upload image service functionality

---
+++ client/src/features/post/services/uploadImage.ts
@@ -0,0 +1,28 @@
+import { POST } from "../../../constants/http";
+
+export const uploadImage = async (ev: React.FormEvent<HTMLFormElement>) => {
+  const form = ev.currentTarget;
+  const fileInput: any = Array.from(form.elements).find(
+    (element: any) => element.name === "file"
+  );
+
+  const formData = new FormData();
+
+  for (const file of fileInput.files) {
+    formData.append("file", file);
+  }
+  formData.append(
+    "upload_preset",
+    `${process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET}`
+  );
+
+  const data = await fetch(`${process.env.REACT_APP_CLOUDINARY_URL}`, {
+    method: POST,
+    body: formData,
+  }).then((res) => res.json());
+
+  return {
+    imageUrl: data.secure_url,
+    data: data,
+  };
+};
