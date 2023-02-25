aab91b776ee87321b78e79abc416622cb74966fe
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-02-25 18:31:39 +0200

    Create error constants

---
+++ server/src/constants/errors.js
@@ -0,0 +1,4 @@
+export const PASSWORDS_DONT_MATCH = 'Passwords don\'t match';
+export const USER_ALREADY_EXISTS = 'User already exists';
+export const USER_DOESNT_EXIST = 'User doesn\'t exist';
+export const INVALID_CREDENTIALS = 'Invalid credentials'
