00bc294ce716341ffa6cb351b01c8d7a33f5c662
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-01 00:31:09 +0200

    Fix footer component

--- client/src/layouts/footer/Footer.tsx
+++ client/src/layouts/footer/Footer.tsx
@@ -1,41 +1,97 @@
+import { Link } from "react-router-dom";
+
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+import appStore from "../../assets/app-store.png";
+import googlePlay from "../../assets/google-play.png";
+
 import "./Footer.scss";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <section className="footer__section">
-        <ul className="footer__list--main">
+        <ul className="footer__list--main main__list">
           <ul className="footer__list--child child">
             <li className="footer__li--item li__heading">COMPANY</li>
-            <li className="footer__li--item">About Us</li>
-            <li className="footer__li--item">Careers</li>
-            <li className="footer__li--item">Terms</li>
-            <li className="footer__li--item">Privacy</li>
-            <li className="footer__li--item">Interet Based Ads</li>
-            <li className="footer__li--item">Ad Preferences</li>
-            <li className="footer__li--item">Help</li>
+            <li className="footer__li--item child__item">
+              <Link to={"/"}>About Us</Link>
+            </li>
+            <li className="footer__li--item child__item">
+              <Link to={"/"}>Careers</Link>
+            </li>
+            <li className="footer__li--item child__item">
+              <Link to={"/"}>Terms</Link>
+            </li>
+            <li className="footer__li--item child__item">
+              <Link to={"/"}>Privacy</Link>
+            </li>
+            <li className="footer__li--item child__item">
+              <Link to={"/"}>Interet Based Ads</Link>
+            </li>
+            <li className="footer__li--item child__item">
+              <Link to={"/"}>Ad Preferences</Link>
+            </li>
+            <li className="footer__li--item child__item">
+              <Link to={"/"}>Help</Link>
+            </li>
           </ul>
           <ul className="footer__list--child child">
             <li className="footer__li--item li__heading">WORK WITH US</li>
-            <li className="footer__li--item">Swirk Creators</li>
-            <li className="footer__li--item">Advertise</li>
-            <li className="footer__li--item">Authors & Ads Blog</li>
-            <li className="footer__li--item">API</li>
+            <li className="footer__li--item child__item">
+              <Link to={"/"}>Swirk Creators</Link>
+            </li>
+            <li className="footer__li--item child__item">
+              <Link to={"/"}>Advertise</Link>
+            </li>
+            <li className="footer__li--item child__item">
+              <Link to={"/"}>Authors & Ads Blog</Link>
+            </li>
+            <li className="footer__li--item child__item">
+              <Link to={"/"}>API</Link>
+            </li>
           </ul>
           <ul className="footer__list--child child social__icons">
-            <li className="footer__li--item li__heading">FB</li>
-            <li className="footer__li--item">TWITTER</li>
-            <li className="footer__li--item">INSTAGRA</li>
-            <li className="footer__li--item">LINKEDIN</li>
+            <li className="footer__li--item child__item icon__li">
+              <Link to={"/"} className="footer__link--social social__link">
+                <FacebookIcon />
+              </Link>
+            </li>
+            <li className="footer__li--item child__item icon__li">
+              <Link to={"/"} className="footer__link--social social__link">
+                <TwitterIcon />
+              </Link>
+            </li>
+            <li className="footer__li--item child__item icon__li">
+              <Link to={"/"} className="footer__link--social social__link">
+                <InstagramIcon />
+              </Link>
+            </li>
+            <li className="footer__li--item child__item icon__li">
+              <Link to={"/"} className="footer__link--social social__link">
+                <LinkedInIcon />
+              </Link>
+            </li>
           </ul>
         </ul>
         <ul className="footer__list">
-          <ul>
-            <li className="footer__list--item shop app__store"></li>
-            <li className="footer__list--item shop google__play"></li>
+          <ul className="footer__list--stores stores">
+            <li className="footer__list--item shop app__store">
+              <Link to={"https://www.apple.com/app-store/"}>
+                <img src={appStore} alt="App Store" loading="lazy" />
+              </Link>
+            </li>
+            <li className="footer__list--item shop google__play">
+              <Link to={"https://play.google.com/store/games"}>
+                <img src={googlePlay} alt="Google Play" loading="lazy" />
+              </Link>
+            </li>
           </ul>
           <li className="footer__list--copy copyright">
-            {new Date().getFullYear()} Swirk, Inc.
+            &copy; {new Date().getFullYear()} Swirk, Inc.
           </li>
         </ul>
       </section>
