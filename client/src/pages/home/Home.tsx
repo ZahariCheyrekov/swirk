705469d20656893650750d3d01f9d0f403e9c9dc
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-03 12:22:14 +0200

    Create post section and import component

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import SideNavigation from "../../components/navigation/SideNavigation";
+import Post from "../../components/post/Post";
 import Search from "../../components/search/Search";
 import Trends from "../../components/trends/Trends";
 
@@ -9,6 +10,9 @@ const Home = () => {
     <main className="main__home">
       <section className="home__section--nav nav">
         <SideNavigation />
+      </section>
+      <section className="home__posts">
+        <Post />
       </section>
       <section className="home__section--trends trends">
         <section className="search__section">
