2b924cbe39355f3b448319f271375f571861c312
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-12 16:13:54 +0200

    Fix edit profile content

--- client/src/features/edit/components/EditProfile.tsx
+++ client/src/features/edit/components/EditProfile.tsx
@@ -8,17 +8,31 @@ const EditProfile = () => {
       <main className="main__edit">
         <section className="edit__section">
           <h2 className="edit__heading--name edit__heading">Edit profile</h2>
+          <article className="edit__article--images article__images">
+            <input
+              type="file"
+              name="file"
+              className="edit__form--file edit__file file"
+            />
+            <img
+              src="https://res.cloudinary.com/dhcdh9u9h/image/upload/v1677363416/swirk/rf92ofpoxb8fgbl0fmel.jpg"
+              alt="John Doe"
+              className="edit__img--background img__background"
+            />
+            <div className="profile__image--wrapper image__wrapper">
+              <img
+                src="https://res.cloudinary.com/dhcdh9u9h/image/upload/v1677363416/swirk/rf92ofpoxb8fgbl0fmel.jpg"
+                alt="John Doe"
+                className="edit__img--photo img__photo"
+              />
+              <input
+                type="file"
+                name="file"
+                className="edit__form--file edit__file file"
+              />
+            </div>
+          </article>
           <form action="POST" className="edit__profile--form edit__form">
-            <input
-              type="file"
-              name="file"
-              className="edit__form--file edit__file file"
-            />
-            <input
-              type="file"
-              name="file"
-              className="edit__form--file edit__file file"
-            />
             <input
               type="text"
               name="name"
@@ -27,7 +41,7 @@ const EditProfile = () => {
             />
             <textarea
               name="bio"
-              placeholder="What do you think?"
+              placeholder="Bio"
               className="edit__form--textarea edit__bio bio textarea"
             />
             <span className="edit__span--buttons span__buttons">
