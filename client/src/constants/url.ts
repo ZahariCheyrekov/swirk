a4e0cb1fe44372827ace8683680662cbd198d4d2
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-13 23:22:39 +0200

    Create posts url

--- client/src/constants/url.ts
+++ client/src/constants/url.ts
@@ -3,4 +3,5 @@ export const BASE_URL = "http://localhost:5000";
 export const LOGIN_URL = `${BASE_URL}/login`;
 export const REGISTER_URL = `${BASE_URL}/register`;
 
+export const GET_POSTS_URL = `${BASE_URL}/posts`;
 export const CREATE_POST_URL = `${BASE_URL}/create`;
