3753f3b50b5302756fd5d214e4d8898a5537e3b7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-13 23:23:26 +0200

    Implement get posts functionality

--- server/src/controllers/post-controller.js
+++ server/src/controllers/post-controller.js
@@ -1,5 +1,16 @@
 import * as postService from '../services/post-service.js';
 
+export const getPosts = async (req, res) => {
+    try {
+        const posts = await postService.getMainPosts();
+
+        console.log(posts);
+
+        return res.status(200).json(posts);
+    } catch (error) {
+        return res.status(500).json({ message: error });
+    }
+}
 
 export const createPost = async (req, res) => {
     const { postContent, imageSrc, userId } = req.body;
@@ -7,7 +18,7 @@ export const createPost = async (req, res) => {
     try {
         const post = await postService.createPost({ postContent, imageSrc }, userId);
 
-        return res.status(200).json(post);
+        return res.status(201).json(post);
 
     } catch (error) {
         return res.status(500).json({ message: error });
