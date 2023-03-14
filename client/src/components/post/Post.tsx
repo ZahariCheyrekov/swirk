617467cf42cf231ce27374ed78ef8053dca4e917
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-14 23:46:51 +0200

    Fix post content data

--- client/src/components/post/Post.tsx
+++ client/src/components/post/Post.tsx
@@ -5,22 +5,24 @@ import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 
 import dummyData from "./dummy.json";
+import { IPost } from "../../interfaces/Post";
 
 import "./Post.scss";
 
-const Post = () => {
+const Post = ({ post }: { post: IPost }) => {
   const navigate = useNavigate();
 
   const handlePostClick = () => {
     navigate(`/${dummyData.nickname}/${dummyData.postId}`);
   };
+  console.log(post);
 
   return (
     <article className="post__article" onClick={handlePostClick}>
       <aside className="post__aside aside__image">
         <img
-          src={dummyData.image}
-          alt={dummyData.name}
+          src={post.imageSrc}
+          alt={post.imageSrc}
           className="post__image"
           loading="lazy"
         />
@@ -35,7 +37,7 @@ const Post = () => {
           <h3 className="post__heading--time">{dummyData.time}</h3>
         </article>
         <p className="post__paragraph--cotent post__content paragraph content">
-          {dummyData.content}
+          {post.postContent}
         </p>
         <section className="post__section--actions section__actions actions">
           <ul className="post__list--actions">
@@ -43,7 +45,7 @@ const Post = () => {
               <span className="post__span--action action__comments comments">
                 <ChatBubbleOutlineOutlinedIcon className="post__icon--comment comment__icon post__icon" />
                 <span className="post__span--commentscount comments__count">
-                  {dummyData.comments.length}
+                  {post?.comments?.length}
                 </span>
               </span>
             </li>
@@ -51,7 +53,7 @@ const Post = () => {
               <span className="post__span--action action__reswirks swirks">
                 <ShareOutlinedIcon className="post__icon--reswirk reswirk__icon post__icon" />
                 <span className="post__span--reswirkscount reswirks__count">
-                  {dummyData.reswirk.length}
+                  {post?.reswirks?.length}
                 </span>
               </span>
             </li>
@@ -59,7 +61,7 @@ const Post = () => {
               <span className="post__span--action action__likes likes">
                 <FavoriteBorderOutlinedIcon className="post__icon--like like__icon post__icon" />
                 <span className="post__span--likescount likes__count">
-                  {dummyData.likes.length}
+                  {post?.likes?.length}
                 </span>
               </span>
             </li>
