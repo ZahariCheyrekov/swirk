4770d20bc36bede6242fe5a0102eb746a174a2af
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-16 00:11:23 +0200

    Configure undo reswirk post

--- server/src/routes/post-routes.js
+++ server/src/routes/post-routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getPosts, createPost, likePost, dislikePost } from '../controllers/post-controller.js';
+import { getPosts, createPost, likePost, dislikePost, undoReswirk } from '../controllers/post-controller.js';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get("/posts", getPosts);
 router.post("/create", createPost);
 router.post("/posts/like", likePost);
 router.delete("/posts/dislike", dislikePost);
+router.delete("/posts/undo-reswirk", undoReswirk);
 
 export default router;
