e7d94da50c03457cd4d5a99166cddfdadd8a9528
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-15 23:37:24 +0200

    Configure dislike post route

--- server/src/routes/post-routes.js
+++ server/src/routes/post-routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { getPosts, createPost, likePost } from '../controllers/post-controller.js';
+import { getPosts, createPost, likePost, dislikePost } from '../controllers/post-controller.js';
 
 const router = express.Router();
 
 router.get("/posts", getPosts);
 router.post("/create", createPost);
 router.post("/posts/like", likePost);
+router.delete("/posts/dislike", dislikePost);
 
 export default router;
