e60469e79e1c6749c46d7067a8f7a9f4e3e796d9
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-15 22:53:51 +0200

    Implement like post by user

--- server/src/services/user-service.js
+++ server/src/services/user-service.js
@@ -1,9 +1,17 @@
 import User from '../models/User.js';
 
-
 export const createUserPost = (userId, postId) => {
     return User.findByIdAndUpdate(
         { _id: userId },
         { $push: { createdPosts: postId } },
+        { runValidators: true }
+    );
+}
+
+export const likeUserPost = (userId, postId) => {
+    return User.findByIdAndUpdate(
+        { _id: userId },
+        { $push: { likedPosts: postId } },
+        { runValidators: true }
     );
 }
