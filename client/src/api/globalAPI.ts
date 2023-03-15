af75f02f3224b07c10b501e8628ad3b6a1aa56dc
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-15 22:54:57 +0200

    Create like post request

--- client/src/api/globalAPI.ts
+++ client/src/api/globalAPI.ts
@@ -1,6 +1,8 @@
 import * as requester from "./requester";
 
-import { GET_POSTS_URL } from "../constants/url";
-
+import { GET_POSTS_URL, LIKE_POST_URL } from "../constants/url";
 
 export const getPosts = () => requester.get(GET_POSTS_URL);
+
+export const likePost = (postId: string, userId: string) =>
+  requester.post(LIKE_POST_URL, { postId, userId });
