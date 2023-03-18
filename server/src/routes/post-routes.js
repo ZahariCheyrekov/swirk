90f36ae286eee7ab10d38c0021410e1ec9e0dff3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-18 15:50:01 +0200

    Configure post routes

--- server/src/routes/post-routes.js
+++ server/src/routes/post-routes.js
@@ -2,9 +2,11 @@ import express from 'express';
 
 import {
     getPosts,
+    getPost,
     createPost,
     likePost,
     dislikePost,
+    commentPost,
     reswirkPost,
     undoReswirk
 } from '../controllers/post-controller.js';
@@ -12,9 +14,11 @@ import {
 const router = express.Router();
 
 router.get("/posts", getPosts);
+router.get("/posts/:postId", getPost);
 router.post("/create", createPost);
 router.post("/posts/like", likePost);
 router.delete("/posts/dislike", dislikePost);
+router.post("/posts/comment", commentPost);
 router.post("/posts/reswirk", reswirkPost);
 router.delete("/posts/undo-reswirk", undoReswirk);
 
