ef4ac41519006984fa625bbf45c209adf8bcb72d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-18 16:47:40 +0200

    Add post requests

--- client/src/features/post/api/post-api.ts
+++ client/src/features/post/api/post-api.ts
@@ -1,6 +1,23 @@
 import * as requester from "../../../api/requester";
+import * as urls from "../../../constants/url";
 
-import { IPostCreate } from "../../../interfaces/Post";
-import { CREATE_POST_URL } from "../../../constants/url";
+import { IPostCreate, IPostCreated } from "../../../interfaces/Post";
 
-export const createPost = (data: IPostCreate) => requester.post(CREATE_POST_URL, data);
+export const getPost = (postId: string | undefined) =>
+  requester.get(`${urls.GET_POSTS_URL}/${postId}`);
+
+export const getPostComments = (
+  postId: string | undefined,
+  postComments: IPostCreated[] | undefined
+) => requester.post(urls.GET_POST_COMMENTS_URL, { postId, postComments });
+
+export const createPost = (data: IPostCreate) =>
+  requester.post(urls.CREATE_POST_URL, data);
+
+export const commentPost = (
+  postData: IPostCreate, 
+  postId: string | undefined,
+  userId: string | undefined
+) => {
+  requester.post(urls.COMMENT_POST_URL, { postData, postId, userId });
+};
