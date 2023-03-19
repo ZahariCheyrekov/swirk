8a67dc9c5eb1dff32a056bbaa7bed51306d56404
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-19 18:47:37 +0200

    Fix post api

--- client/src/features/post/api/post-api.ts
+++ client/src/features/post/api/post-api.ts
@@ -1,23 +1,21 @@
 import * as requester from "../../../api/requester";
 import * as urls from "../../../constants/url";
 
-import { IPostCreate, IPostCreated } from "../../../interfaces/Post";
+import { IPostCreate } from "../../../interfaces/Post";
 
 export const getPost = (postId: string | undefined) =>
   requester.get(`${urls.GET_POSTS_URL}/${postId}`);
 
 export const getPostComments = (
   postId: string | undefined,
-  postComments: IPostCreated[] | undefined
+  postComments: IPostCreate[] | undefined
 ) => requester.post(urls.GET_POST_COMMENTS_URL, { postId, postComments });
 
 export const createPost = (data: IPostCreate) =>
   requester.post(urls.CREATE_POST_URL, data);
 
 export const commentPost = (
-  postData: IPostCreate, 
+  postData: IPostCreate,
   postId: string | undefined,
   userId: string | undefined
-) => {
-  requester.post(urls.COMMENT_POST_URL, { postData, postId, userId });
-};
+) => requester.post(urls.COMMENT_POST_URL, { postData, postId, userId });
