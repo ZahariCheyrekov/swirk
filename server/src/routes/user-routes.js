6910b3c686d6a5a12ab6e7fb20448c50c265600d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-20 23:45:57 +0200

    Configure get likes route

--- server/src/routes/user-routes.js
+++ server/src/routes/user-routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { getUser, getSwirks } from '../controllers/user-controller.js';
+import { getUser, getSwirks, getLikes, getComments } from '../controllers/user-controller.js';
 
 
 const router = express.Router();
 
 router.get("/user/:nickname", getUser);
 router.get("/user/:userId/swirks", getSwirks)
+router.get("/user/:userId/likes", getLikes)
 
 export default router;
