b4f51914ffc237fa27642815437b4e6e7a13c180
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-03-20 23:08:21 +0200

    Configure get posts request

--- client/src/features/profile/api/user-api.ts
+++ client/src/features/profile/api/user-api.ts
@@ -3,3 +3,6 @@ import { BASE_URL } from "../../../constants/url";
 
 export const getUserData = (userNickname: string) =>
   requester.get(`${BASE_URL}/user/${userNickname}`);
+
+export const getCreatedPosts = (userId: string) =>
+  requester.get(`${BASE_URL}/user/${userId}/swirks`);
