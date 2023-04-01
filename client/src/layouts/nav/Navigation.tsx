230456d96b00ed2445c3079f908056cddc085962
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-04-01 10:44:27 +0300

    Implement toggle navigation logic

--- client/src/layouts/nav/Navigation.tsx
+++ client/src/layouts/nav/Navigation.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 
 import MenuIcon from "@mui/icons-material/Menu";
@@ -7,12 +7,20 @@ import swirkLogo from "../../assets/swirk-dark.png";
 import { UserContext } from "../../contexts/UserContext";
 
 import "./Navigation.scss";
+import useScrollLock from "../../hooks/useScrollLock";
 
 const Navigation = () => {
   const { user } = useContext(UserContext);
+  const { toggleScrollLock } = useScrollLock();
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const handleMenuClick = () => {
+    setMenuOpened((prevState) => !prevState);
+    toggleScrollLock();
+  };
 
   return (
-    <section className="navigation__section">
+    <section className={`navigation__section ${menuOpened && "open"}`}>
       <nav className="navigation__nav">
         <article className="navigation__article">
           <img
@@ -21,8 +29,11 @@ const Navigation = () => {
             className="navigation__img--logo img__logo"
           />
         </article>
-        <MenuIcon className="nav__menu--icon menu__icon" />
-        <ul className="nav__list">
+        <MenuIcon
+          className="nav__menu--icon menu__icon"
+          onClick={handleMenuClick}
+        />
+        <ul className={`nav__list ${menuOpened && "open"}`}>
           <li className="nav__li--item">
             <Link to={"/home"}>Home</Link>
           </li>
