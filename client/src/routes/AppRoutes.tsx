2da03aa549c57363899610417f0be133fba41fb2
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-04-01 14:56:38 +0300

    Configure guest and user routes

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -11,25 +11,38 @@ import Bookmarks from "../features/bookmarks/components/Bookmarks";
 import Profile from "../features/profile/components/Profile";
 import EditProfile from "../features/edit/components/EditProfile";
 import PostDetails from "../features/post/components/PostDetails";
+import NotFound from "../pages/404/NotFound";
+
+import GuestGuard from "../guards/GuestGuard";
+import UserGuard from "../guards/UserGuard";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Welcome />} />
       <Route path="/home" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/login" element={<Auth />} />
-      <Route path="/register" element={<Auth />} />
       <Route path="/create" element={<CreatePost />} />
       <Route path="/explore" element={<Explore />} />
       <Route path="/search" element={<Search />} />
       <Route path="/bookmarks" element={<Bookmarks />} />
-      <Route path="/:nickname" element={<Profile />} />
-      <Route path="/:nickname/likes" element={<Profile />} />
-      <Route path="/:nickname/comments" element={<Profile />} />
-      <Route path="/:nickname/shares" element={<Profile />} />
-      <Route path="/:nickname/edit" element={<EditProfile />} />
-      <Route path="/:nickname/:postId" element={<PostDetails />} />
+      <Route path="/profile/:nickname" element={<Profile />} />
+      <Route path="/profile/:nickname/likes" element={<Profile />} />
+      <Route path="/profile/:nickname/comments" element={<Profile />} />
+      <Route path="/profile/:nickname/shares" element={<Profile />} />
+      <Route path="/profile/:nickname/edit" element={<EditProfile />} />
+      <Route path="/profile/:nickname/:postId" element={<PostDetails />} />
+
+      <Route element={<UserGuard />}>
+        <Route path="/create" element={<CreatePost />} />
+        <Route path="/profile/:nickname/edit" element={<EditProfile />} />
+      </Route>
+
+      <Route element={<GuestGuard />}>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Auth />} />
+        <Route path="/register" element={<Auth />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
