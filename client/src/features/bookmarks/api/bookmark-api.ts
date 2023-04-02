cbb299f83a36a73d2399229dffb83026f13924e3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-04-02 21:59:53 +0300

    Fix book api

--- client/src/features/bookmarks/api/bookmark-api.ts
+++ client/src/features/bookmarks/api/bookmark-api.ts
@@ -1,5 +1,5 @@
 import * as requester from "../../../api/requester";
 import { BASE_URL } from "../../../constants/url";
 
-export const getBookmarks = (userId: string | undefined) =>
+export const getBookmarks = (userId: string) =>
   requester.get(`${BASE_URL}/user/${userId}/bookmarks`);
