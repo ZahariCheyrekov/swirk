e8c943507db2f646de222f8f9bde209d3a5edb89
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-04-06 00:26:33 +0300

    Update search component functionality

--- client/src/features/search/components/Search.tsx
+++ client/src/features/search/components/Search.tsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 import SearchField from "../../../components/search/SearchField";
 import SideNavigation from "../../../components/navigation/SideNavigation";
 import Trends from "../../../components/trends/Trends";
 
+import { getUsers } from "../api/search-api";
+
 import "./Search.scss";
 
 const Search = () => {
+  const { nickname } = useParams();
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const users = await getUsers(nickname);
+      setSearchResults(users);
+    };
+    fetchUsers();
+  }, [nickname]);
+
   return (
     <main className="search__main">
       <section className="search__section--nav nav">
@@ -14,6 +30,12 @@ const Search = () => {
         <section className="search__section--trends trends">
           <section className="search__section">
             <SearchField />
+
+            {searchResults.length > 0 ? (
+              <ul></ul>
+            ) : (
+              <h3>No users with such nickname</h3>
+            )}
           </section>
         </section>
       </section>
